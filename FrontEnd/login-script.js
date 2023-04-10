3c309175c6c60e9f7fc142bc294642f5ae1e8820
maxmart26 <torgue.maxime@gmail.com>
2023-04-10 11:20:55 +0200

    avec les differentes login

--- FrontEnd/login-script.js
+++ FrontEnd/login-script.js
@@ -12,17 +12,32 @@ button.addEventListener("click", async (e) => {
     }
     console.log(login);
     try {
+        supp()
         const reponse = await fetch("http://localhost:5678/api/users/login", {
             method: "POST",
             headers: { 'Content-Type': 'application/json'},
             body: JSON.stringify(login)
         })
-        const result = reponse.json()
-        console.log("succes", result);
+        console.log("succes", reponse.status);
+        if(reponse.status == 200)//vas a la page de connection
+        {
+
+        }
+        else if (reponse .status == 401)//lui indique que un probleme de connection
+        {
+            let display =""
+            display += `votre email ou votre mot de passe est incorrect`
+                document.querySelector("#erreur").insertAdjacentHTML("beforeend",display)
+        }
     }
     catch (error) {
         console.log("grosnull", error)
     }
 })
 
-
+function supp(){ 
+    let erreur = document.getElementById("erreur");
+    while (erreur[0].firstChild) {
+        erreur[0].removeChild(erreur[0].firstChild);
+    }
+}
